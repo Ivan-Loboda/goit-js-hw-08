5fb29e76b4995d74f765c3b49de57ed0daa69fa5
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-09-30 19:32:59 +0300

    .

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+// import VimeoPlayer from "@vimeo/player";
+import throttle from "lodash.throttle"
+// console.log(trotle)
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Vimeo.Player(iframe);
+
+const onPlay = function (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+    // console.log(onPlay)
+};
+
+iframePlayer.on('timeupdate', throttle(onPlay, 1000));
+
+
+const playerCurrenTime = localStorage.getItem('videoplayer-current-time');
+
+
+
+iframePlayer.setCurrentTime(playerCurrenTime);
