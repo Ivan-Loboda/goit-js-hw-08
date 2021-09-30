08f7104d0a9a3a9a5635fdf095caef1c2ac99330
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-09-30 18:24:10 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,6 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-// console.log(galleryItems);
 const galleryBox = document.querySelector('.gallery');
 
 const markUp = galleryItems.map(item =>
