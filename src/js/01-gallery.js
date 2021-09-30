507325f268c5ca60b660a3cce3d9174fdc032d08
Ivan-Loboda <Ivan.Loboda.1996@gmail.com>
2021-09-30 18:19:00 +0300

    .

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,3 +7,22 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 // console.log(galleryItems);
+const galleryBox = document.querySelector('.gallery');
+
+const markUp = galleryItems.map(item =>
+    `<li>
+    <a class="gallery__link" href="${item.original}" style ="cursor: default">
+                <img class="gallery__image"
+                    src="${item.preview}"
+                    alt="${item.description}"
+                />
+            </a>
+    </li>`).join("");
+
+
+galleryBox.insertAdjacentHTML('beforeend', markUp);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+});
